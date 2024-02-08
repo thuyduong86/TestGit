d210218b599259a3bdd2e49e889d0560eabe0ee0
thuyduong86 <thuy.duong161086@gmail.com>
2024-02-08 16:40:23 +0100

    Estoy probando github en visual studio

--- buenosdia.java
+++ buenosdia.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class main{
     public static void main{
-        System.out.print("Buenos dias");
+        //System.out.print("Buenos dias");
+        System.out.print("Estoy probando github con visual studio");
     }
    
 }
